309d992325465d40dc0db9695a664464822aa01e
Jun Li <junli@Juns-MacBook-Pro.local>
2019-07-02 16:43:35 -0700

    add googlenet

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,7 @@
 from .resnet import *
 from .vgg import *
+
+import torchvision.models as models
+
+def GoogleNet(num_classes, pretrained=False):
+    return models.googlenet(num_classes, pretrained)
