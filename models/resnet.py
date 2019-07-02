3139b5fa18b86ac1a3566ba0b52b67a233e70c11
Jun Li <junli19860919@gmail.com>
2019-07-02 16:35:42 -0700

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -1,8 +1,13 @@
 import torchvision.models as models
-
+  
 def ResNet(num_classes, depth, pretrained=False):
     if depth == 18:
         return models.resnet18(pretrained, num_classes=num_classes)
-
-
-# resnet18 = models.resnet18(pretrained=False)
+    elif depth == 34:
+        return models.resnet34(pretrained, num_classes=num_classes)
+    elif depth == 50:
+        return models.resnet50(pretrained, num_classes=num_classes)
+    elif depth == 101:
+        return models.resnet101(pretrained, num_classes=num_classes)
+    elif depth == 152:
+        return models.resnet152(pretrained, num_classes=num_classes)
