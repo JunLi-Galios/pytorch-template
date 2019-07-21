51651506f4e53cac5ec640cb2a037a21d3aa5602
Jun Li <junli19860919@gmail.com>
2019-07-21 10:42:18 -0700

    Delete pytorch_optimizers.py

--- optimizers/pytorch_optimizers.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-def SGD(parameters, learning_rate, momentum, weight_decay):
-    return torch.optim.SGD(parameters, lr=learning_rate, momentum=momentum, nesterov=True, weight_decay=weight_decay)
-
-def ADAM(parameters, learning_rate, momentum, weight_decay):
-    return torch.optim.Adam(parameters, lr=learning_rate, betas=(momentum, 0.999), eps=1e-08, weight_decay=weight_decay, amsgrad=False)
